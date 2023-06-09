dc9f5d0ab2820bcbc8295715e1f9d9313282f03d
Cstrp <valery.nosareu@gmail.com>
2023-06-09 18:35:55 +0300

    upd

--- server/src/models/user.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const userSchema = new mongoose_1.Schema({
-    userName: { type: String, required: true, unique: false },
-});
-exports.default = (0, mongoose_1.model)("Users", userSchema);
