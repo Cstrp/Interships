fc48b66137cec6b0f74fdb2434e5463459c7b630
Cstrp <valery.nosareu@gmail.com>
2023-06-16 11:12:03 +0300

    revert

--- server/src/controllers/colletionsController.ts
+++ server/src/controllers/colletionsController.ts
@@ -4,7 +4,6 @@ import Collections from "../models/collection";
 import { User } from "../types";
 import { uploadImage } from "../utils/uploadImage";
 import Item from "../models/item";
-import { Buffer } from "buffer";
 
 const getCollections = async (req: Request, res: Response) => {
   try {
@@ -19,37 +18,31 @@ const getCollections = async (req: Request, res: Response) => {
 const createCollection = async (req: Request, res: Response) => {
   try {
     const user = req.user as User;
-    const { name, description, theme, fields } = req.body;
+    const { imageUrl, name, description, theme, fields } = req.body;
 
     if (!user) {
       errorHandler(res, 401, "Unauthorized!");
       return;
     }
 
-    if (req.file) {
-      const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let image;
 
-      const imageUrl = "data:" + req.file.mimetype + ";base64," + b64;
-
-      if (imageUrl) {
-        await uploadImage(imageUrl);
-      }
-
-      const newCollection = await new Collections({
-        userId: user._id,
-        name,
-        description,
-        theme,
-        imageUrl,
-        fields,
-      }).save();
-
-      // ПРОВЕРИТЬ ЗАГРУЗКУ ФАЙЛОВ!!!!!!!!!!
-
-      res
-        .status(201)
-        .json({ message: "New collection has been created!", newCollection });
+    if (imageUrl) {
+      image = await uploadImage(imageUrl);
     }
+
+    const newCollection = await new Collections({
+      userId: user._id,
+      name,
+      description,
+      theme,
+      imageUrl: image,
+      fields,
+    }).save();
+
+    res
+      .status(201)
+      .json({ message: "New collection has been created!", newCollection });
   } catch (err) {
     errorHandler(res, 500, `Internal Server Error ${err}`);
   }
