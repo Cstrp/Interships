00ec4f717f5bd6f3ad9cf8ac76bc1983a46e654b
Cstrp <valery.nosareu@gmail.com>
2023-07-05 21:15:13 +0300

    upd

--- client/src/data/api/apiUrl.ts
+++ client/src/data/api/apiUrl.ts
@@ -1,4 +1,4 @@
-const dev = "http://localhost:8080/api";
+// const dev = "http://localhost:8080/api";
 const prod = "https://itransitioncourse-production.up.railway.app/api";
 
 export const apiUrl = prod;
